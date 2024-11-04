a6fbcbbd5a27d538b54705b7da172879d750184b
Sam <linming1007@gmail.com>
2024-11-04 21:14:15 +0800

    feat: rcli csv parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+
+use clap::Parser;
+use rcli::process_csv;
+use rcli::{Opts, SubCommand};
+
+//rcli csv -i input.csv -o output.json --header -d ','
+
+
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            process_csv(&opts.input, &opts.output)?;
+        }
+    }
+    anyhow::Ok(())
 }
